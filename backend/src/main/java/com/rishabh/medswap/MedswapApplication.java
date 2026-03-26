741bcbcece2111e706944363baca5d67f74d7e2b
RishabhS-dev <rishabhsinghprofessional@gmail.com>
2026-03-26 10:56:27 +0530

    initialized Structure

---
+++ backend/src/main/java/com/rishabh/medswap/MedswapApplication.java
@@ -0,0 +1,13 @@
+package com.rishabh.medswap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedswapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedswapApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/rishabh/medswap/MedswapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rishabh.medswap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedswapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
